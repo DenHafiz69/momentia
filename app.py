9c0abaf5da8f29a4424d56bd273d1593faed5125
Den Hafiz <den.m.hafiz@gmail.com>
2024-12-13 22:22:05 +0800

    Add register route

--- app.py
+++ app.py
@@ -1,8 +1,15 @@
-from flask import Flask, render_template, redirect
+from flask import Flask, render_template, redirect, request
 
 app  = Flask(__name__)
 
 # The homepage of the website
-@app.route("/")
+@app.route('/')
 def index():
-    return render_template("index.html")
+    return render_template('index.html')
+
+
+# User that is not logged in would see this and the login only
+@app.route('/register', methods=['POST', 'GET'])
+def register():
+    if request.method == 'GET':
+        return render_template('register.html')
