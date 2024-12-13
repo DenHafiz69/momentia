674b07de102b3e5ccdc21022950e12e3cff0fb9f
Den Hafiz <den.m.hafiz@gmail.com>
2024-12-14 00:07:38 +0800

    Copy from CS50 code for login and logout

--- app.py
+++ app.py
@@ -1,14 +1,52 @@
-from flask import Flask, render_template, redirect, request
-import flask_login
+import os
 
+from flask import Flask, render_template, redirect, request, session
+from flask_session import Session
+from werkzeug.security import check_password_hash, generate_password_hash
+
+# Configure application
 app  = Flask(__name__)
-login_manaer = flask_login.LoginManager()
-login_manaer.init_app(app)
+
+# Configure session to use filesystem (instead of signed cookies)
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
 
 # The homepage of the website
 @app.route('/')
 def index():
+
     return render_template('index.html')
 
 # Login when the user already in the database
 # If not, go to the index page and tell the user to login
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+
+    # Forget any user_id
+    session.clear()
+
+    if request.method == 'GET':
+        return render_template('login.html')
+
+    elif request.method == 'POST':
+        # Ensure username was submmited
+        if not request.form.get('username'):
+            return render_template('apology.html', error="must provide username")
+
+        # Ensure password was submmited
+        if not request.form.get('password'):
+            return render_template('apology.html', error="must provide password")
+
+        # Ensure password was submmited
+
+        # Query database for username
+
+# Logout the current user
+@app.route('/logout')
+def logout():
+    
+    # Forget any user_id
+    session.clear()
+
+    return redirect('/')
