e5ecc0659f4fcd8ecf8001e3717fbaa069a2ff12
Den Hafiz <den.m.hafiz@gmail.com>
2025-01-05 22:38:48 +0800

    Include flash message

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import os
 
-from flask import Flask, render_template, redirect, request, session
+from flask import Flask, render_template, redirect, request, session, url_for, flash
 from flask_session import Session
 from werkzeug.security import check_password_hash, generate_password_hash
 
@@ -43,24 +43,20 @@ def index():
 @app.route('/login', methods=['POST', 'GET'])
 def login():
 
-    msg = ''
-
     if request.method == 'POST' and 'username' in request.form and 'password' in request.form:
         username = request.form['username']
         password = request.form['password']
 
-        cursor.execute('SELECT * FROM accounts WHERE username=? AND password=?', username, password)
+        cursor.execute('SELECT * FROM accounts WHERE username=? AND password=?', (username, password))
         account = cursor.fetchone()
         if account:
-            session['loggedin'] = True
-            session['id'] = account['id']
             session['username'] =account['username']
-            msg = 'Logged in successfully!'
-            return render_template('index.html', msg = msg)
+            flash('Logged in successfully!')
+            return render_template('index.html', notification = notification)
         else:
-            msg = 'Incorrect username / password'
+            flash('Invalid credentials')
         
-    return render_template('login.html', msg = msg)
+    return render_template('login.html')
         
 
 # Logout the current user
