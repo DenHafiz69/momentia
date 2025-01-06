13eaf6d5f9a73da92dae2e874cf1037cb1ab2eb0
Den Hafiz <den.m.hafiz@gmail.com>
2025-01-06 17:17:24 +0800

    Import from forms module

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ import os
 import sqlite3
 import cs50
 
+from forms import RegistrationForm, LoginForm
+
 from werkzeug.security import check_password_hash, generate_password_hash
 from flask import Flask, render_template, redirect, session, flash, g
 from flask_session import Session
@@ -10,9 +12,8 @@ from flask_session import Session
 # Configure application
 app  = Flask(__name__)
 
-# Configure session to use filesystem (instead of signed cookies)
-app.config["SESSION_PERMANENT"] = False
-app.config["SESSION_TYPE"] = "filesystem"
+app.config['SECRET_KEY'] = '1c61ded0bfaa415caea6fe5916ebf59d'
+
 Session(app)
 
 DATABASE = "./data/data.db"
@@ -37,41 +38,15 @@ def index():
     # Show the homepage
     return render_template('index.html')
 
-# Login when the user already in the database
-# If not, go to the index page and tell the user to login
 @app.route('/login', methods=['POST', 'GET'])
 def login():
+    form = LoginForm()
+    return render_template('login.html', title='Login', form=form)
 
-    if request.method == 'POST' and 'username' in request.form and 'password' in request.form:
-        username = request.form['username']
-        password = request.form['password']
-
-        rows = db.execute("""
-            SELECT * FROM users
-            WHERE username = ?
-        """, username)[0]
-
-        if rows:
-            user_id = rows['id']
-            username = rows['username']
-            password_hash = rows['password']
-
-            try:
-                check_password_hash(password_hash, password)
-            except:
-                flash("Password does not match.")
-            
-            session["user_id"] = user_id
-            
-            flash("Login successful")
-
-            return redirect("/")
-            
-        else:
-            flash("Invalid credentials.")
-    
-    return render_template('login.html')
-        
+@app.route('/register', methods=['POST', 'GET'])
+def register():
+    form = RegistrationForm()
+    return render_template('register.html', title='Register', form=form)
 
 # Logout the current user
 @app.route('/logout')
@@ -82,30 +57,7 @@ def logout():
 
     return redirect('/')
 
-@app.route('/register', methods=['POST', 'GET'])
-def register():
 
-    if request.method == 'POST':
-
-        username = request.form.get('username')
-        password = generate_password_hash(request.form.get('password'))
-
-        rows = db.execute("""
-            SELECT * FROM users
-            WHERE username=?
-        """, (username))
-
-        if rows:
-            flash("User already exist.")
-        else:
-            db.execute("""
-                INSERT INTO users (username, password)
-                VALUES(?,?)
-            """, username, password)
-
-            return redirect('/')
-
-    return render_template('register.html')
 
 @app.route('/settings', methods=['POST', 'GET'])
 def settings():
