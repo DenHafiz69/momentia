222cd86107c3b1f2cd95c7c57a8a566da45796f7
Den Hafiz <den.m.hafiz@gmail.com>
2025-01-07 09:35:52 +0800

    Add session scope

--- modules/database.py
+++ modules/database.py
@@ -1,36 +1,37 @@
+# database_module.py
 import sqlalchemy as db
+from sqlalchemy.orm import scoped_session, sessionmaker
 
 # Database setup
-engine = db.create_engine("sqlite:///../data/database.db", echo=True)
-connection = engine.connect()
+engine = db.create_engine("sqlite:///:memory:", echo=True, connect_args={"check_same_thread": False})
+Session = scoped_session(sessionmaker(bind=engine))
+
+# Metadata and table definition
 metadata = db.MetaData()
 
-# Table definition
 user_table = db.Table(
     "user",
     metadata,
     db.Column("id", db.Integer, primary_key=True),
     db.Column("username", db.String),
     db.Column("email", db.String),
-    db.Column("password", db.String)
+    db.Column("password", db.String),
 )
 
-# Create the table in the database
 metadata.create_all(engine)
 
-# Functions for interacting with the database
+# Functions for database operations
 def insert_user(username: str, email: str, password: str) -> None:
+    session = Session()
     query = user_table.insert().values(username=username, email=email, password=password)
-    connection.execute(query)
+    session.execute(query)
+    session.commit()
+    session.close()
 
-def select_user(username: str) -> db.engine.Row:
+def select_user(username: str):
+    session = Session()
     query = user_table.select().where(user_table.c.username == username)
-    result = connection.execute(query)
-    return result.fetchone()
-    
-def main() -> None:
-    metadata.create_all(engine)
-    connection.close()
-
-if __name__ == "__main__":
-    main()
+    result = session.execute(query)
+    user = result.fetchone()
+    session.close()
+    return user
