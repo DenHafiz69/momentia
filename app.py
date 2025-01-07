b65d387375e65f78cb5069066b27da497a4bf413
Den Hafiz <den.m.hafiz@gmail.com>
2025-01-07 09:17:58 +0800

    Import modules

--- app.py
+++ app.py
@@ -8,40 +8,12 @@ from werkzeug.security import check_password_hash, generate_password_hash
 from flask import Flask, render_template, redirect, request, session, url_for, flash, g
 from flask_session import Session
 
-from flask_sqlalchemy import SQLAlchemy
-
-db = SQLAlchemy()
+from modules.database import insert_user, select_user
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '1c61ded0bfaa415caea6fe5916ebf59d'
 app.config['SESSION_TYPE'] = "filesystem"
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///./data/database.db'
-
-db.init_app(app)
-
-with app.app_context():
-    db.create_all()
-
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(20), unique=True, nullable=False)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
-    password = db.Column(db.String(60), nullable=False)
-    posts = db.relationship('Post', backref='author', lazy=True)
-
-    def __repr__(self):
-        return f"User('{self.username}', '{self.email}', '{self.image_file}')"
-
-class Post(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(100), nullable=False)
-    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow())
-    content = db.Column(db.Text, nullable=False)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-
-    def __repr__(self):
-        return f"Post('{self.title}', '{self.date_posted}')"
 
 posts = [
     {
